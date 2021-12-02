adbe82e3cd169068b0af85ae665a083acd7f3608
sakamuuy <sakamuu.y@gmail.com>
2021-12-02 21:03:31 +0900

    Add piece

---
+++ src/components/Piece/Piece.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+import type { Props as ContainerProps} from '.'
+
+export type Props = ContainerProps & {
+  belongColor: 'WHITE' | 'BLACK',
+
+}
+
+export const Presentation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/Piece/index.tsx
@@ -0,0 +1,12 @@
+import { Presentation } from "./Piece"
+
+export type Props = {
+  belongColor: 'WHITE' | 'BLACK',
+  
+}
+
+export const Piece = () => {
+  return (
+    <Presentation />
+  )
+}
