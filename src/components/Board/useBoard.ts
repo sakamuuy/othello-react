f2497ee726b0536788e8c996e6841097ba5fb893
sakamuuy <sakamuu.y@gmail.com>
2021-12-03 13:32:19 +0900

    Add onclick

--- src/components/Board/useBoard.ts
+++ src/components/Board/useBoard.ts
@@ -1,9 +1,9 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { Props } from './Board'
 
 const arr8 = Array(8).fill(0)
 export const useBoard = (): Props => {
-  const [currentPlayer, setCurrentPlayer] = useState<Props['currentPlayer']>('Player1')
+  const currentPlayer = useRef<Props['currentPlayer']>('Player1')
   const [squarePropsArr, setSquarePropsArr] = useState<Props['squarePropsArr']>(arr8.map(() => arr8.map(() => ({isSet: false}))))
 
   useEffect(() => {
@@ -36,12 +36,12 @@ export const useBoard = (): Props => {
 
   // Return null when the result is out of the board
   const getNormalizedState = (row:number, col:number): AroundState | null => {
-    return (row < 0 || row > 7 || col < 0 || col > 7 )? 
+    return (row < 0 || row > 7 || col < 0 || col > 7 )? null :
       {
         ...squarePropsArr[row][col],
         row,
         col
-      } : null
+      }
   }
 
   const getAroundEnemiesDiscs = (row: number, col: number) => {
@@ -55,19 +55,20 @@ export const useBoard = (): Props => {
       getNormalizedState(row + 1, col),
       getNormalizedState(row + 1, col + 1),
     ]
-    return arroundStatus.filter((status): status is NonNullable<AroundState> => status !== null && status?.isSet && status.player !== currentPlayer)
+
+    return arroundStatus.filter((status): status is NonNullable<AroundState> => status !== null && status?.isSet && status.player !== currentPlayer.current)
   }
 
   const getCanSandwitchDiscs = (row: number, col: number, foundEnemieDisc: AroundState): AroundState[] => {
     const xDir = foundEnemieDisc.col - col
     const yDir = foundEnemieDisc.row - row
     let nextX = col + xDir
-    let nextY = col + yDir 
+    let nextY = row + yDir 
     const canSandwitchDiscs: AroundState[] = []
 
     while (1) {
       const nextDirDisc = getNormalizedState(nextY, nextX)
-      if (nextDirDisc !== null && nextDirDisc.isSet && nextDirDisc.player !== currentPlayer) {
+      if (nextDirDisc !== null && nextDirDisc.isSet && nextDirDisc.player !== currentPlayer.current) {
         canSandwitchDiscs.push(nextDirDisc)
         nextX += xDir
         nextY += yDir
@@ -81,16 +82,33 @@ export const useBoard = (): Props => {
 
   const onClickSquare = useCallback((row: number, col: number) => {
     const aroundEnemiesDiscs: AroundState[] = getAroundEnemiesDiscs(row, col)
+    
     if (aroundEnemiesDiscs.length === 0) return;
 
     const canSandwitchDiscs = aroundEnemiesDiscs.map((disc) => getCanSandwitchDiscs(row, col, disc))
-    
 
+    const tmp = [...squarePropsArr]
+    tmp[row][col] = {
+      ...tmp[row][col],
+      isSet: true,
+      player: currentPlayer.current
+    }
+    canSandwitchDiscs.forEach((dir) => {
+      dir.forEach((disc) => {
+        tmp[disc.row][disc.col] = {
+          ...tmp[disc.row][disc.col],
+          player: currentPlayer.current
+        }
+      })
+    })
+    setSquarePropsArr(tmp)
+
+    currentPlayer.current = currentPlayer.current === 'Player2'? 'Player1' : 'Player2'
   }, [squarePropsArr, setSquarePropsArr])
 
   return {
     squarePropsArr,
-    currentPlayer,
+    currentPlayer: currentPlayer.current,
     onClickSquare
   }
 }
