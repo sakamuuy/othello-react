2fb86bf0263fb7a2f20b639f678b979a6c86d0c4
sakamuuy <sakamuu.y@gmail.com>
2021-12-03 13:36:20 +0900

    Disc design

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,10 +1,27 @@
 import type { Props } from '.'
 import './index.css'
 
+type DiscProps = {
+  color: 'black' | 'white'
+}
+const Disc = ({ color }: DiscProps) => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        background: color,
+      }}
+    >
+    </div>
+  )
+}
+
 export const Presentation = ({ isSet, player, onClickSquare, row, col }: Props) => {
   return (
     <div className="square" onClick={() => onClickSquare(row, col)}>
-      {isSet? player === "Player1"? 'black' : 'white' : null}
+      {isSet? player === "Player1"? <Disc color="black" /> : <Disc color='white' /> : null}
     </div>
   )
 }
